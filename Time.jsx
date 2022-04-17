3625301ecc3ac0371f412b06f90bbcc2cbd9dc7a
Kumar Shivam <98182315+kumarshivam01@users.noreply.github.com>
2022-04-17 13:18:36 +0530

    done

--- Time.jsx
+++ Time.jsx
@@ -1,35 +1,61 @@
-import React, { useEffect, useState } from 'react'
-import '../Timer.css'
-const Timer = ()=>{
-  const [Time,setTime] = useState(0)
-  const [Timeron, setTimeon]=useState(false)
-  useEffect(()=>{
-    let interval = null
-    if(Timeron){
+import React, { useEffect, useState } from "react";
+import "../Timer.css";
+const Timer = () => {
+  const [Time, setTime] = useState(0);
+  const [Timeron, setTimeon] = useState(false);
+  useEffect(() => {
+    let interval = null;
+    if (Timeron) {
       interval = setInterval(() => {
-        setTime(prevTime=>prevTime+10)
+        setTime((prevTime) => prevTime + 10);
       }, 10);
+    } else {
+      clearInterval(interval);
     }
-    else{
-      clearInterval(interval)
-    }
-    return()=>clearInterval(interval)
-  },[Timeron])
-  return(
-    
-    <div>
-      <div>
-        <span>{("0"+Math.floor((Time/60000)%60)).slice(-2)}:</span>
-        <span>{("0"+Math.floor((Time/1000)%60)).slice(-2)}:</span>
-        <span>{("0"+((Time/10)%100)).slice(-2)}</span>
+    return () => clearInterval(interval);
+  }, [Timeron]);
+  return (
+    <div className="main">
+      <div className="timer">
+        <span>{("0" + Math.floor((Time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((Time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + ((Time / 10) % 100)).slice(-2)}</span>
       </div>
-      <div>
-        <button onClick={()=>{setTimeon(true)}}>Start</button>
-        <button onClick={()=>{setTimeon(false)}}>Stop</button>
-        <button onClick={()=>{setTimeon(true)}}>Resume</button>
-        <button onClick={()=>{setTime(0)}}>Reset</button>
+      <div className="control">
+        <button
+          className="start"
+          onClick={() => {
+            setTimeon(true);
+          }}
+        >
+          Start
+        </button>
+        <button
+          className="stop"
+          onClick={() => {
+            setTimeon(false);
+          }}
+        >
+          Stop
+        </button>
+        <button
+          className="resume"
+          onClick={() => {
+            setTimeon(true);
+          }}
+        >
+          Resume
+        </button>
+        <button
+          className="reset"
+          onClick={() => {
+            setTime(0);
+          }}
+        >
+          Reset
+        </button>
       </div>
     </div>
-  )
-}
-export default Timer
+  );
+};
+export default Timer;
